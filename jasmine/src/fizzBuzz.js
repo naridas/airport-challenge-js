02847f18f22442c05a4b768eb5dc4ae6bbaac43c
naridas <gregsparta@gmail.com>
2016-05-09 15:47:19 +0100

    jasmine

---
+++ jasmine/src/fizzBuzz.js
@@ -0,0 +1,12 @@
+function fizbuzz() {
+  if (number % 15 === 0){
+    return 'FizzBuzz';
+  }
+  if (number % 5 === 0){
+    return 'Buzz';
+  }
+  if (number % 3 === 0){
+    return 'Fizz';
+  }
+  return number;
+}
