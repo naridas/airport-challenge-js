7195d16e426ce1e90431ed3c52d139ce01616d78
naridas <gregsparta@gmail.com>
2016-05-10 18:27:50 +0100

    DI weather fix errors

--- src/Airport.js
+++ src/Airport.js
@@ -1,5 +1,6 @@
   "use strict";
-function Airport() {
+function Airport(weather) {
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
   this._hangar = [];
 
 }
@@ -9,11 +10,14 @@ Airport.prototype.planes = function() {
 };
 
 Airport.prototype.clearForLanding = function(plane) {
+  if(this._weather.isStormy()) {
+    throw new Error('cannot land during storm');
+  }
   this._hangar.push(plane);
 };
 
 Airport.prototype.clearForTakeOff = function() {
-  if(this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot takeoff during storm');
   }
   this._hangar.pop();
